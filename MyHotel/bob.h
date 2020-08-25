b8b08ca515306e9f7e132b252ee82f01859b2e69
voidrealms <bcairns@voidrealms.com>
2020-08-25 13:34:43 -0400

    first commit

---
+++ MyHotel/bob.h
@@ -0,0 +1,19 @@
+#ifndef BOB_H
+#define BOB_H
+
+#include <QObject>
+#include <QDebug>
+
+class Bob : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Bob(QObject *parent = nullptr);
+    void fixit(QString room, QString thing);
+signals:
+
+private:
+    int m_times;
+};
+
+#endif // BOB_H
